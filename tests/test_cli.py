325ff5fcb3f9f32e414fcf639ba1888447bc3956
zackees <zachvorhies@protonmail.com>
2023-01-24 11:46:46 -0500

    update

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,15 @@
-import sys
+"""
+Unit test file.
+"""
 import unittest
 import os
 
+
 class MainTester(unittest.TestCase):
+    """Main tester class."""
+
     def test_imports(self) -> None:
+        """Test command line interface (CLI)."""
         rtn = os.system("test_cmd")
         self.assertEqual(0, rtn)
 
