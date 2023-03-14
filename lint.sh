a8d94adfb99729b7ac5236f0dc763e108d6ef2c2
Zachary Vorhies <zachvorhies@protonmail.com>
2023-03-14 16:39:37 -0700

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -5,7 +5,7 @@ cd $( dirname ${BASH_SOURCE[0]})
 echo Running black src tests
 black src tests
 echo Running isort src tests
-isort src tests
+isort --profile black src tests
 echo Running flake8 src tests
 flake8 src tests
 echo Running pylint src
